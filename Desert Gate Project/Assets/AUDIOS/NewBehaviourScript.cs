3cc9482e02fd7f5e69f38f32ba090ecb24054d63
David Ruiz Luengo <99996753+xdavido@users.noreply.github.com>
2023-05-25 17:59:10 +0200

    final

---
+++ Desert Gate Project/Assets/AUDIOS/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource playSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        playSound.Play();
+    }
+}
